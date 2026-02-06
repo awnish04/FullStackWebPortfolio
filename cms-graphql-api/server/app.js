8e2cb5d84ddf8d499e352e0d62317d2db57abbc5
AWNISH MEHTA <awnishmehta69@gmail.com>
2026-02-06 19:56:48 +0545

    third CORS configuration update Deployment

--- cms-graphql-api/server/app.js
+++ cms-graphql-api/server/app.js
@@ -1,49 +1,3 @@
-// import express from "express";
-// import cors from "cors";
-// import { ApolloServer } from "@apollo/server";
-// import { expressMiddleware } from "@apollo/server/express4";
-
-// import { typeDefs } from "../typeDefs/index.js";
-// import { resolvers } from "../resolver/index.js";
-
-// const app = express();
-
-// const corsOptions = {
-//   origin: [
-//     "http://localhost:3000",
-//     "http://localhost:3001",
-//     // "https://web-portfolio-i7zdsqnwt-awnish04s-projects.vercel.app",
-//     // "https://portfolio-dashboard-main-quyr4ujfb-awnish04s-projects.vercel.app",
-//   ],
-//   credentials: true,
-// };
-
-// const server = new ApolloServer({
-//   typeDefs,
-//   resolvers,
-//   introspection: true,
-// });
-
-// let started = false;
-
-// export async function createApp() {
-//   if (!started) {
-//     await server.start();
-//     started = true;
-
-//     app.use(
-//       "/api/graphql",
-//       cors(corsOptions),
-//       express.json(),
-//       expressMiddleware(server),
-//     );
-
-//     app.get("/health", (_, res) => res.json({ status: "OK" }));
-//   }
-
-//   return app;
-// }
-
 import express from "express";
 import cors from "cors";
 import { ApolloServer } from "@apollo/server";
@@ -59,7 +13,7 @@ const allowedOrigins = [
   "http://localhost:3000", // frontend local
   "http://localhost:3001", // alternate dev port
   "https://welcome-to-awnish-mehta-web-portfol.vercel.app", // deployed frontend
-  "https://admin-dashboard-awnish-mehta-web-portfolio-ihqi37wmy.vercel.app", // deployed admin dashboard
+  "https://admin-dashboard-awnish-mehta-web-git-c92fa2-awnish04s-projects.vercel.app", // deployed admin dashboard
 ];
 
 const corsOptions = {
